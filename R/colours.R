e5854cfb289a7a73fe7cb75cbe2ca922ec7f5884
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2023-05-12 15:15:14 +0100

    export colour functions

--- R/colours.R
+++ R/colours.R
@@ -1,85 +1,132 @@
+#' @title Transparent Colour Creator
+#'
+#' @description Defining a transparent version of a given colour.
+#'
+#' @param name A valid colour name, as listed by [colors()].
+#' @param rgb A numeic vector of RGB values.
+#' @param maxValue The maximum colour value, default to 1.
+#' @param alpha The level of transparency
+#'
+#' @return A character vector of the hexidecimal value defining the colour.
+#'
+#' @export
+#'
+#' @examples
+#' myBlue = alphaCol(0.5, name="blue")
+#' myRed = alphaCol(0.1, rgb=c(240, 20, 2-), maxValue=255)
+alphaCol = function(alpha, name = NULL, rgb = NULL, maxValue=1) {
+  if (is.null(name) && is.null(rgb)) {
+    stop("Either a valid colour name of a vector RGB values must be passed.")
+  }
+  if (!is.null(name)) {
+    rgb = as.vector(col2rgb(name))
+  }
+  return(rgb(rgb[1]/maxValue, rgb[2]/maxValue, rgb[3]/maxValue, alpha))
+}
 
-#' Transparent Black
+
+#' @title Transparent Black
 #'
-#' Create a black with a specified level of opacity.
+#' @description Create a black with a specified level of opacity.
 #'
-#' The RGB values for alphaBlack are 0,0,0.
+#' @details The RGB values for alphaBlack are 0,0,0.
 #'
 #' @param alpha The level of opacity, between 0.0 and 1.0, 0.0 being completely
 #' transparent and 1.0 being opaque.
 #'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 alphaBlack = function(alpha) rgb(0,0,0, alpha)
 
 
-#' Transparent Blue
+#' @title Transparent Blue
 #'
-#' Create a blue with a specified level of opacity.
+#' @description Create a blue with a specified level of opacity.
 #'
-#' The RGB values used for alphaBlue are 0,0 and 128, where the maximum is 255.
+#' @details The RGB values used for alphaBlue are 0,0 and 128, where the maximum is 255.
 #'
 #' @param alpha The level of opacity, between 0.0 and 1.0, 0.0 being completely transparent and 1.0 being opaque.
+#'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 alphaBlue = function(alpha) rgb(0,0,128/255, alpha)
 
 
-#' Transparent Red
+#' @title Transparent Red
 #'
-#' Create a red with a specified level of opacity.
+#' @description Create a red with a specified level of opacity.
 #'
-#' The RGB values for alphaRed are 255,0 and 0, where the maximum is 255.
+#' @details The RGB values for alphaRed are 255,0 and 0, where the maximum is 255.
 #'
 #' @param alpha The level of opacity, between 0.0 and 1.0, 0.0 being completely transparent and 1.0 being opaque.
+#'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 alphaRed = function(alpha) rgb(1,0,0, alpha)
 
 
-#' Transparent Green
+#' @title Transparent Green
 #'
-#' Create a green with a specified level of opacity.
+#' @description Create a green with a specified level of opacity.
 #'
-#' The RGB values used for alphaGreen are 0,150 and 0, where the maximum is 255.
+#' @details The RGB values used for alphaGreen are 0,150 and 0, where the maximum is 255.
 #'
 #' @param alpha The level of opacity, between 0.0 and 1.0, 0.0 being completely transparent and 1.0 being opaque.
+#'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 alphaGreen = function(alpha) rgb(0,150/255,0, alpha)
 
 
-#' Transparent Pink
+#' @title Transparent Pink
 #'
-#' Create a pink with a specified level of opacity.
+#' @description Create a pink with a specified level of opacity.
 #'
-#' The RGB values for alphaPink are 255,62 and 150, where the maximum is 255.
+#' @details The RGB values for alphaPink are 255,62 and 150, where the maximum is 255.
 #'
 #' @param alpha The level of opacity, between 0.0 and 1.0, 0.0 being completely transparent and 1.0 being opaque.
+#'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 alphaPink = function(alpha) rgb(255/255,62/255,150/255, alpha)
 
-
-#' Blue-Red Colour Ramp
+#' @title Blue-Red Colour Ramp
 #'
-#' For creating transitions colours between red and blue, with a given transparency.
+#' @description For creating transitions colours between red and blue, with a given transparency.
 #'
-#' The colour gradient lies between [analysis2Dmito::alphaBlue()] and [analysis2Dmito::alphaRed()]
+#' @details The colour gradient lies between [analysis2Dmito::alphaBlue()] and [analysis2Dmito::alphaRed()]
 #'
 #' @param alphaLevel The level of opacity of the two end-point colours. A number between 0.0 and 1.0, where 0.0 is completely transparent and 1.0 being opaque.
+#'
 #' @returns A character object of the hex value colour name created.
+#'
+#' @export
 ramp_BlueRed = function(alphaLevel){
   colorRamp(c(alphaBlue(alphaLevel), alphaRed(alphaLevel)), alpha=TRUE)
 }
 
 
-#' Classification Colour
+#' @title Classification Colour
 #'
+#' @description
 #' The colour given to a fibre given its probability of being deficient. The
 #' colours are on a scale of red to blue where red corresponds to deficiency i.e.
 #' a probability of 1.0.
 #'
+#' @details
 #' The colour gradient is created using [analysis2Dmito::ramp_BlueRed()].
 #'
 #' @param classif A numeric vector for the deficiency probabilities. All elements must be between 0.0 and 1.0.
 #' @param alphaLevel A number between 0.0 and 1.0 to control the level of the opacity of the colours.
+#'
 #' @returns A character vector hex value RGB names.
+#'
+#' @export
 classcols = function(classif, alphaLevel=1.0){
   alphaCramp = ramp_BlueRed(alphaLevel)
   rgbvals = alphaCramp(classif)/255.0
--- R/output_saver.R
+++ R/output_saver.R
@@ -5,15 +5,15 @@
 #' dataframe, as written in the list,  onto the end of a root file path. The
 #' files are saved as .txt files defaulted to the tab separator.
 #'
+#' @details
+#' The function uses the [write.table()] function to save the files.
+#'
 #' @param df_list A list of dataframes to be saved.
 #' @param root A character vector of the root paths to which the names of the list will be added. If this is a single character then the same root will be used for files.
 #' @param replace A boolean variable. If TRUE the function will overwrite any file that exists under the same name.
 #' @param sep A character of an appropriate separator value.
 #' @param col.names,row.names A boolean, passed directly to [write.table()].
 #' @param ... Any additional parameters to be passed to write.table.
-#'
-#' @details
-#' The function uses the [write.table()] function to save the files.
 #'
 #' @examples
 #' data_list = list(fileOne = data.frame(a=1:20, b=21:40), fileTwo=data.frame(x=1:1000, y=rnorm(1000)))
