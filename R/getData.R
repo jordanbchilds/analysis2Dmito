4bd69f903bcf1523cdd2aa488ebe745a59702100
jordanbchilds <74244328+jordanbchilds@users.noreply.github.com>
2023-06-28 18:25:06 +0100

    getData.R - remove check in parent directory keep
    column names in data changed so keep capital letter at the start

--- R/getData.R
+++ R/getData.R
@@ -8,9 +8,8 @@
 #'
 #' @details
 #' The file is read using the [data.table::fread] function. If the file does not
-#' exist in the path suggested the function check the filepath "../<fname>", if
-#' the file does not exist here then an error is returned stating no such file
-#' exists.
+#' exist here then an error is returned stating as such.
+#'
 #' @details
 #' For NPC correction to take place the `filenames` column in the dataset must
 #' be apprropriately labelled as "<sampleID> NPC" or "<sampleID> OXPHOS"
@@ -28,8 +27,6 @@
 #'
 #' @export
 
-
-
 getData = function(fname,
                    cord,
                    correctnpc = TRUE,
@@ -41,45 +38,36 @@ getData = function(fname,
       stringsAsFactors = FALSE,
       header = TRUE
     )
-    colnames(dat) = tolower(colnames(dat))
-  } else if (file.exists(file.path("..", fname))) {
-    dat = data.table::fread(
-      file.path("..", fname),
-      sep = "\t",
-      stringsAsFactors = FALSE,
-      header = TRUE
-    )
-    colnames(dat) = tolower(colnames(dat))
   } else {
     stop(paste(fname, "does not exist in current or parent directory."))
   }
 
-  dat = dat[dat$ch %in% cord, ]
-  dat$fn = gsub(" NPC", "", dat$filename)
+  dat = dat[dat$Channel %in% cord, ]
+  dat$fn = gsub(" NPC", "", dat$Filename)
   dat$fn = gsub(" OXPHOS", "", dat$fn)
   dat$pch = paste(dat$fn, dat$ch)
 
   if (correctnpc) {
-    npc = dat[grepl("NPC", dat$filename),]
-    oxphos = dat[grepl("OXPHOS", dat$filename),]
-    agg = stats::aggregate(npc$value, by = list(npc$pch), mean)
+    npc = dat[grepl("NPC", dat$Filename),]
+    oxphos = dat[grepl("OXPHOS", dat$Filename),]
+    agg = stats::aggregate(npc$Value, by = list(npc$pch), mean)
     lu = agg$x
     names(lu) = gsub("_L._C._S._R.", "", agg$Group.1)
 
-    oxphos$filename = gsub(" OXPHOS", "", oxphos$filename)
+    oxphos$Filename = gsub(" OXPHOS", "", oxphos$Filename)
 
-    newvals = pmax(1.0, oxphos$value - lu[gsub("_L._C._S._R.", "", oxphos$pch)])
+    newvals = pmax(1.0, oxphos$Value - lu[gsub("_L._C._S._R.", "", oxphos$pch)])
     if (!is.null(noNPCchannels))
       newvals[oxphos$chan %in% noNPCchannels] = oxphos$value[oxphos$chan %in% noNPCchannels]
     oxphos$value = newvals
 
-    oxphos$filename = oxphos$fn
+    oxphos$Filename = oxphos$fn
     oxphos$fn = NULL
     oxphos$pch = NULL
     dat = oxphos
   } else {
-    dat = dat[!grepl("NPC", dat$filename), ]
-    dat$filename = gsub(" OXPHOS", "", dat$filename)
+    dat = dat[!grepl("NPC", dat$Filename), ]
+    dat$Filename = gsub(" OXPHOS", "", dat$Filename)
   }
   dat$type = "Mean intensity"
   return(data.frame(dat, stringsAsFactors = FALSE))
