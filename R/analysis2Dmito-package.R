b0fdae221c365d58256c0ad39598cc04fabafb08
Jordan Childs <74244328+jordanbchilds@users.noreply.github.com>
2023-11-14 12:48:53 +0000

    stan implementation

---
+++ R/analysis2Dmito-package.R
@@ -0,0 +1,16 @@
+#' The 'analysis2Dmito' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name analysis2Dmito-package
+#' @aliases analysis2Dmito
+#' @useDynLib analysis2Dmito, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.3. https://mc-stan.org
+#'
+NULL
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,10 +12,10 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4bhlmm_stan_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4bhlmm_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4bhlmm_stan_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bhlmm_stan_mod, 0},
+    {"_rcpp_module_boot_stan_fit4bhlmm_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bhlmm_mod, 0},
     {NULL, NULL, 0}
 };
 
