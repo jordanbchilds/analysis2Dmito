058d23cc5b7325314f77b11b190e044205fa9e04
Jordan Childs <74244328+jordanbchilds@users.noreply.github.com>
2024-02-01 11:45:12 +0000

    Change default transparency for 2Dmito plot in postPlot.R

--- R/postPlot.R
+++ R/postPlot.R
@@ -260,13 +260,13 @@ postPlot = function(post,
   graphics::points(
     dataMats$ctrl,
     pch = 20,
-    col = analysis2Dmito::alphaBlack(0.05),
+    col = analysis2Dmito::alphaBlack(0.01),
     ...
   )
   graphics::points(
     dataMats$pts,
     pch = 20,
-    col = analysis2Dmito::classcols(classifs),
+    col = analysis2Dmito::classcols(classifs, alphaLevel=0.02),
     ...
   )
 
